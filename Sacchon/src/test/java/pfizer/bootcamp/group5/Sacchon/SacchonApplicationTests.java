bffbd1e7f5108cac43ebbb4ce908d59c9c0cef54
Eleni <elena.v.kalla@gmail.com>
2023-02-09 18:00:54 +0200

    Initial commit

---
+++ Sacchon/src/test/java/pfizer/bootcamp/group5/Sacchon/SacchonApplicationTests.java
@@ -0,0 +1,13 @@
+package pfizer.bootcamp.group5.Sacchon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacchonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
