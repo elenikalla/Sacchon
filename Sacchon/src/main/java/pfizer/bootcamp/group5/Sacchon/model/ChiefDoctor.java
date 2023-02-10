5d35dbe8ea8c45cdae7fe83badd260c74e451e10
stavrosantoniadis <sr.antoniadis@gmail.com>
2023-02-10 20:41:18 +0200

    Maven dependencies + first models

--- Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/model/ChiefDoctor.java
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/model/ChiefDoctor.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
-public class Patient {
+public class ChiefDoctor {
     private int id;
     private String username;
     private String password;
--- Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/model/Doctor.java
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/model/Doctor.java
@@ -1,24 +1,16 @@
 package pfizer.bootcamp.group5.Sacchon.model;
 
-
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
 @Setter
 @Getter
-@Entity
 @AllArgsConstructor
-@NoArgsConstructor
 public class Doctor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int id;
-    String fullName;
-
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "currentDoctor")
-    private Set<Patient> managedPatients;
+    private int id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
 }
