66223b5635a7071cf998fd446edcb6a810aec4a4
Afroditi Xinti <102307107+afxinti@users.noreply.github.com>
2023-02-12 22:16:11 +0200

    initial patient class and repository

---
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/model/Patient.java
@@ -0,0 +1,26 @@
+package pfizer.bootcamp.group5.Sacchon.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String fullName;
+    private int amkaCode;
+    private LocalDate dateOfBirth;
+    private boolean hasRecordings;
+    @ManyToOne (fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name ="doctor_id",nullable=false)
+    private Doctor currentDoctor;
+}
