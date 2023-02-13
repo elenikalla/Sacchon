bba8e824e1b57596e068a50e050727d7a460b8cb
Eleni <elena.v.kalla@gmail.com>
2023-02-13 10:14:05 +0200

    BGL and DCI Repositories - added Repository Annotation

--- Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/repository/BGLRepository.java
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/repository/BGLRepository.java
@@ -1,8 +1,9 @@
 package pfizer.bootcamp.group5.Sacchon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pfizer.bootcamp.group5.Sacchon.model.BloodGlucoseLevel;
-
+@Repository
 public interface BGLRepository extends JpaRepository<BloodGlucoseLevel,Integer> {
 
 }
--- Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/repository/DCIRepository.java
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/repository/DCIRepository.java
@@ -1,7 +1,8 @@
 package pfizer.bootcamp.group5.Sacchon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pfizer.bootcamp.group5.Sacchon.model.DailyCarbonatesIntake;
-
+@Repository
 public interface DCIRepository extends JpaRepository<DailyCarbonatesIntake,Integer> {
 }
