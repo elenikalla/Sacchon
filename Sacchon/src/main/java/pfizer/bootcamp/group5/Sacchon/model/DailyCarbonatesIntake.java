b51b2f3007acc48c0ce19dd74aafa6161c0c27e0
Eleni <elena.v.kalla@gmail.com>
2023-02-13 11:44:49 +0200

    Created BGL and DCI models

---
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/model/DailyCarbonatesIntake.java
@@ -0,0 +1,32 @@
+package pfizer.bootcamp.group5.Sacchon.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="DCI_table")
+public class DailyCarbonatesIntake {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="DCI_id")
+    private int id;
+    @Column(name="DCI_date")
+    private LocalDate date;
+    @Column(name="DCI_time")
+    private LocalTime time;
+    @Column(name="DCI_measurement")
+    private Double measurement;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="fk_patient_id",nullable=false)
+    private Patient patient;
+}
