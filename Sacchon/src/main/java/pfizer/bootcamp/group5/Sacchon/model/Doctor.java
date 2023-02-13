bc86607cde888d7bb244abef9218b8c4a5727a13
Afroditi Xinti <102307107+afxinti@users.noreply.github.com>
2023-02-13 02:07:17 +0200

    small change

---
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/model/Doctor.java
@@ -0,0 +1,24 @@
+package pfizer.bootcamp.group5.Sacchon.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String fullName;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "currentDoctor")
+    private List<Patient> managedPatients;
+}
