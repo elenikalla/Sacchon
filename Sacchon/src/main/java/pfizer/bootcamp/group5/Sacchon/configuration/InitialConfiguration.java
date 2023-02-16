0512ef32f75989bcd06174a2df5e7895df71c1ec
Eleni <elena.v.kalla@gmail.com>
2023-02-16 14:40:46 +0200

    merge config and models from middle

--- Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/configuration/InitialConfiguration.java
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/configuration/InitialConfiguration.java
@@ -12,18 +12,18 @@ import java.util.List;
 
 @Configuration
 public class InitialConfiguration {
-    @Bean
-    CommandLineRunner commandLineRunner(
-            DoctorRepository docRepository, ChiefDoctorRepository chiefDocRepository){
-        return args -> {
-            Doctor doctor1 = new Doctor( 0, "giorgos@gmail.com", "giorgosantoniou","Giorgos", "Antoniou");
-            Doctor doctor2 = new Doctor( 0, "kostas@gmail.com", "kostasparaskevas", "Kostas", "Paraskevas");
-            docRepository.saveAll(List.of(doctor1, doctor2));
-            ChiefDoctor chiefdoctor1 = new ChiefDoctor( 0, "mixalis@gmail.com", "mixaliskarageorgiou","Mixalis", "Karageorgiou");
-            ChiefDoctor chiefdoctor2  = new ChiefDoctor( 0, "nikos@gmail.com", "nikospantiris","Nikos", "Pantiris");
-            chiefDocRepository.saveAll(  List.of(chiefdoctor1, chiefdoctor2)   );
-
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            DoctorRepository docRepository, ChiefDoctorRepository chiefDocRepository){
+//        return args -> {
+//            Doctor doctor1 = new Doctor( 0, "giorgos@gmail.com", "giorgosantoniou","Giorgos", "Antoniou");
+//            Doctor doctor2 = new Doctor( 0, "kostas@gmail.com", "kostasparaskevas", "Kostas", "Paraskevas");
+//            docRepository.saveAll(List.of(doctor1, doctor2));
+//            ChiefDoctor chiefdoctor1 = new ChiefDoctor( 0, "mixalis@gmail.com", "mixaliskarageorgiou","Mixalis", "Karageorgiou");
+//            ChiefDoctor chiefdoctor2  = new ChiefDoctor( 0, "nikos@gmail.com", "nikospantiris","Nikos", "Pantiris");
+//            chiefDocRepository.saveAll(  List.of(chiefdoctor1, chiefdoctor2)   );
+//
+//        };
+//    }
 
 }
