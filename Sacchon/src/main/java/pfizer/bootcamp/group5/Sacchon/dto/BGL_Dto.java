f1b17a087228b2d36622b32b5c0b61b61d4f3b49
Eleni <elena.v.kalla@gmail.com>
2023-02-19 20:50:00 +0200

    Implementing BGL and DCI Dto

---
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/dto/BGL_Dto.java
@@ -0,0 +1,40 @@
+package pfizer.bootcamp.group5.Sacchon.dto;
+
+import pfizer.bootcamp.group5.Sacchon.model.BloodGlucoseLevel;
+import pfizer.bootcamp.group5.Sacchon.model.Patient;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class BGL_Dto {
+
+    private int id;
+    private LocalDate date;
+    private LocalTime time;
+    private Double measurement;
+    private Patient patient;
+
+
+    public BGL_Dto(BloodGlucoseLevel bloodGlucoseLevel) {
+        if (bloodGlucoseLevel != null) {
+            id = bloodGlucoseLevel.getId();
+            date = bloodGlucoseLevel.getDate();
+            time = bloodGlucoseLevel.getTime();
+            measurement = bloodGlucoseLevel.getMeasurement();
+            patient = bloodGlucoseLevel.getPatient();
+        }
+    }
+
+    public BloodGlucoseLevel asBGL() {
+        BloodGlucoseLevel bloodGlucoseLevel = new BloodGlucoseLevel();
+        bloodGlucoseLevel.setId(id);
+        bloodGlucoseLevel.setDate(date);
+        bloodGlucoseLevel.setTime(time);
+        bloodGlucoseLevel.setMeasurement(measurement);
+        bloodGlucoseLevel.setPatient(patient);
+
+        return bloodGlucoseLevel;
+    }
+}
+
+
---
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/dto/DCI_Dto.java
@@ -0,0 +1,37 @@
+package pfizer.bootcamp.group5.Sacchon.dto;
+
+import pfizer.bootcamp.group5.Sacchon.model.DailyCarbonatesIntake;
+import pfizer.bootcamp.group5.Sacchon.model.Patient;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DCI_Dto {
+    private int id;
+    private LocalDate date;
+    private LocalTime time;
+    private Double measurement;
+    private Patient patient;
+
+
+    public DCI_Dto(DailyCarbonatesIntake dailyCarbonatesIntake) {
+        if (dailyCarbonatesIntake != null) {
+            id = dailyCarbonatesIntake.getId();
+            date = dailyCarbonatesIntake.getDate();
+            time = dailyCarbonatesIntake.getTime();
+            measurement = dailyCarbonatesIntake.getMeasurement();
+            patient = dailyCarbonatesIntake.getPatient();
+        }
+    }
+
+    public DailyCarbonatesIntake asDCI() {
+        DailyCarbonatesIntake dailyCarbonatesIntake = new DailyCarbonatesIntake();
+        dailyCarbonatesIntake.setId(id);
+        dailyCarbonatesIntake.setDate(date);
+        dailyCarbonatesIntake.setTime(time);
+        dailyCarbonatesIntake.setMeasurement(measurement);
+        dailyCarbonatesIntake.setPatient(patient);
+
+        return dailyCarbonatesIntake;
+    }
+}
