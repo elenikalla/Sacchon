251ceec0a96c90c539ec2676a9004a0ceb51dbe6
Eleni <elena.v.kalla@gmail.com>
2023-02-20 10:17:24 +0200

    MediDataVaultServices completed crud methods

--- Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/service/MediDataVaultServices.java
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/service/MediDataVaultServices.java
@@ -4,6 +4,7 @@ import pfizer.bootcamp.group5.Sacchon.dto.BGL_Dto;
 import pfizer.bootcamp.group5.Sacchon.dto.ConsultationDto;
 import pfizer.bootcamp.group5.Sacchon.dto.DCI_Dto;
 import pfizer.bootcamp.group5.Sacchon.exception.MyException;
+import pfizer.bootcamp.group5.Sacchon.exception.RecordNotFoundException;
 import pfizer.bootcamp.group5.Sacchon.model.BloodGlucoseLevel;
 
 import java.util.List;
@@ -11,11 +12,11 @@ import java.util.List;
 public interface MediDataVaultServices {
     BGL_Dto createBGL(BGL_Dto bgl);
     List<BGL_Dto> readBGL();
-    BGL_Dto readBGL(int id) throws MyException;
+    BGL_Dto readBGL(int id) throws RecordNotFoundException;
     boolean updateBGL(BGL_Dto consultation, int id);
 
-    BGL_Dto createDCI(DCI_Dto dci);
+    DCI_Dto createDCI(DCI_Dto dci);
     List<DCI_Dto> readDCI();
-    DCI_Dto readDCI(int id) throws MyException;
+    DCI_Dto readDCI(int id) throws RecordNotFoundException;
     boolean updateDCI(DCI_Dto dci_dto, int id);
 }
--- Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/service/MediDataVaultServicesImpl.java
+++ Sacchon/src/main/java/pfizer/bootcamp/group5/Sacchon/service/MediDataVaultServicesImpl.java
@@ -4,53 +4,108 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pfizer.bootcamp.group5.Sacchon.dto.BGL_Dto;
 import pfizer.bootcamp.group5.Sacchon.dto.DCI_Dto;
-import pfizer.bootcamp.group5.Sacchon.exception.MyException;
+import pfizer.bootcamp.group5.Sacchon.exception.RecordNotFoundException;
 import pfizer.bootcamp.group5.Sacchon.model.BloodGlucoseLevel;
 import pfizer.bootcamp.group5.Sacchon.model.DailyCarbonatesIntake;
 import pfizer.bootcamp.group5.Sacchon.repository.BGLRepository;
 import pfizer.bootcamp.group5.Sacchon.repository.DCIRepository;
-
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
-
-public class MediDataVaultServicesImpl {
+public class MediDataVaultServicesImpl implements MediDataVaultServices{
     private final BGLRepository BGLRepository;
     private final DCIRepository DCIRepository;
 
-
+@Override
     public BGL_Dto createBGL(BGL_Dto bgl_dto) {
         BloodGlucoseLevel bloodGlucoseLevel = bgl_dto.asBGL();
         return new BGL_Dto(BGLRepository.save(bloodGlucoseLevel)) ;
     }
+    @Override
 
     public List<BGL_Dto> readBGL() {
-        return null;
+        return BGLRepository
+                .findAll()
+                .stream()
+                .map(BGL_Dto::new)
+                .collect(Collectors.toList());
+    }
+    @Override
+
+    public BGL_Dto readBGL(int id) throws RecordNotFoundException {
+        return new BGL_Dto( readBGL_DB(id));
     }
 
-    public BGL_Dto readBGL(int id) throws MyException {
-        return null;
+    private BloodGlucoseLevel readBGL_DB(int id) throws RecordNotFoundException {
+        Optional<BloodGlucoseLevel> bglOptional = BGLRepository.findById(id);
+        if (bglOptional.isPresent())
+            return bglOptional.get();
+        throw new RecordNotFoundException("Record not found");
     }
+    @Override
 
-    public boolean updateBGL(BGL_Dto bgl, int id) {
-        return false;
+    public boolean updateBGL(BGL_Dto bgl_dto, int id) {
+        boolean action;
+        try {
+            BloodGlucoseLevel dbBGL = readBGL_DB(id);
+            dbBGL.setDate(bgl_dto.getDate());
+            dbBGL.setTime(bgl_dto.getTime());
+            dbBGL.setMeasurement(bgl_dto.getMeasurement());
+            dbBGL.setPatient(bgl_dto.getPatient());
+            BGLRepository.save(dbBGL);
+            action = true;
+        } catch (RecordNotFoundException e) {
+            action = false;
+        }
+        return action;
+
     }
+    @Override
 
     public DCI_Dto createDCI(DCI_Dto dci_dto) {
         DailyCarbonatesIntake dailyCarbonatesIntake = dci_dto.asDCI();
         return new DCI_Dto(DCIRepository.save(dailyCarbonatesIntake)) ;
     }
+    @Override
 
     public List<DCI_Dto> readDCI() {
-        return null;
-    }
 
-    public DCI_Dto readDCI(int id) throws MyException {
-        return null;
+        return DCIRepository
+                .findAll()
+                .stream()
+                .map(DCI_Dto::new)
+                .collect(Collectors.toList());
     }
+    @Override
+
+    public DCI_Dto readDCI(int id) throws RecordNotFoundException {
+        return new DCI_Dto( readDCI_DB(id));
+
+    }
+    private DailyCarbonatesIntake readDCI_DB(int id) throws RecordNotFoundException {
+        Optional<DailyCarbonatesIntake> dciOptional = DCIRepository.findById(id);
+        if (dciOptional.isPresent())
+            return dciOptional.get();
+        throw new RecordNotFoundException("Record not found");
+    }
+    @Override
 
     public boolean updateDCI(DCI_Dto dci_dto, int id) {
-        return false;
+        boolean action;
+        try {
+            DailyCarbonatesIntake dbDCI = readDCI_DB(id);
+            dbDCI.setDate(dci_dto.getDate());
+            dbDCI.setTime(dci_dto.getTime());
+            dbDCI.setMeasurement(dci_dto.getMeasurement());
+            dbDCI.setPatient(dci_dto.getPatient());
+            DCIRepository.save(dbDCI);
+            action = true;
+        } catch (RecordNotFoundException e) {
+            action = false;
+        }
+        return action;
     }
 }
